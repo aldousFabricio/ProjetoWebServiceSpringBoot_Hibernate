e5e471d1b56b3bfef9b0a2bbb7b151b2b6a96e56
Aldous Cavalcanti <108491289+aldousFabricio@users.noreply.github.com>
2022-08-30 18:09:57 -0300

    Projeto Criado

---
+++ src/test/java/com/aldousCavalcanti/SpringBootJPAHibernate/SpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldousCavalcanti.SpringBootJPAHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
